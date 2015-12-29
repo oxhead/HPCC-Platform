19382daabe5a067436695b935056fe1bebf79b9a
Chin-Jung Hsu <chsu6@ncsu.edu>
2015-12-29 14:57:13 -0500

    This is the folk of our own repository with build name prefix osr.

--- version.cmake
+++ version.cmake
@@ -1,10 +1,10 @@
 ###
 ## Version Information
 ###
-set ( HPCC_PROJECT "community" )
+set ( HPCC_PROJECT "osr" )
 set ( HPCC_MAJOR 6 )
 set ( HPCC_MINOR 0 )
 set ( HPCC_POINT 0 )
-set ( HPCC_MATURITY "rc" )
-set ( HPCC_SEQUENCE 4 )
+set ( HPCC_MATURITY "alpha" )
+set ( HPCC_SEQUENCE 1 )
 ###
